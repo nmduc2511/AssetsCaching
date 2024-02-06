766608a743119481c8049f7af57d9d1f155f529d
nmduc2511 <46123715+nmduc2511@users.noreply.github.com>
2024-02-06 07:43:29 +0700

    init

---
+++ AssetsCaching/ASCache.swift
@@ -0,0 +1,91 @@
+import Foundation
+
+public enum ASCacheType {
+    case ramAndDisk
+    case onlyDisk
+}
+
+public class ASCache: NSObject {
+    public static let shared = ASCache()
+    
+    private let disk: ASDiskCache
+    private let memory: ASMemoryCache
+    private let remote: ASRemoteCache
+    private var didSetDataBlock: ((Data, String, ASCacheType) -> Void)?
+    
+    public var memoryCostLimit: Int = 60 {
+        willSet {
+            memory.costLimit = newValue * 1024 * 1024
+        }
+    }
+    
+    public var diskCostLimit: Int = 60 {
+        willSet {
+            disk.costLimit = newValue * 1024 * 1024
+        }
+    }
+    
+    override init() {
+        memory = ASMemoryCache()
+        memory.costLimit = memoryCostLimit * 1024 * 1024
+        disk = ASDiskCache()
+        disk.costLimit = diskCostLimit * 1024 * 1024
+        remote = ASRemoteCache()
+        super.init()
+        
+        didSetDataBlock = { [weak self] data, key, type in
+            guard let self = self else { return }
+            self.setData(data, key: key, cacheType: type)
+        }
+    }
+    
+    func getImage(_ url: URL,
+                  cacheType type: ASCacheType,
+                  completion: ((Data) -> Void)?) {
+
+        switch type {
+        case .ramAndDisk:
+            if let data = memory
+                .object(key: url.absoluteString) as? Data {
+                completion?(data)
+                return
+            }
+            
+            if let data = disk
+                .object(key: url.absoluteString) as? Data {
+                completion?(data)
+                didSetDataBlock?(data, url.absoluteString, type)
+                return
+            }
+        case .onlyDisk:
+            if let data = disk
+                .object(key: url.absoluteString) as? Data {
+                completion?(data)
+                return
+            }
+        }
+
+        remote.getImage(
+            url: url,
+            onNext: { [weak self] data in
+                guard let self = self else { return }
+                completion?(data)
+                self.setData(data, key: url.absoluteString, cacheType: type)
+            },
+            onError: { error in
+                print("ASImageError: \(error.localizedDescription) - url: \(url.absoluteString)")
+            })
+    }
+    
+    private func setData(_ data: Data,
+                         key: String,
+                         cacheType: ASCacheType) {
+        switch cacheType {
+        case .ramAndDisk:
+            memory.saveObject(data, key: key, cost: data.count)
+            disk.saveObject(data, key: key, cost: data.count)
+        case .onlyDisk:
+            disk.saveObject(data, key: key, cost: data.count)
+        }
+    }
+}
---
+++ AssetsCaching/AssetsCaching.h
@@ -0,0 +1,18 @@
+//
+//  AssetsCaching.h
+//  AssetsCaching
+//
+//  Created by david nguyen on 01/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssetsCaching.
+FOUNDATION_EXPORT double AssetsCachingVersionNumber;
+
+//! Project version string for AssetsCaching.
+FOUNDATION_EXPORT const unsigned char AssetsCachingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetsCaching/PublicHeader.h>
+
+
